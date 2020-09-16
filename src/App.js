e88732a6315df5ec7c34c415cd811e2800173258
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-09-17 01:03:47 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { NavLink, Route } from "react-router-dom";
+import User from "./containers/Users/Users";
+import Courses from "./containers/Courses/Courses";
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<nav>
+				<NavLink to="/user">User </NavLink>
+				<NavLink to="/courses">Courses</NavLink>
+			</nav>
+			<Route path="/user" component={User} />
+			<Route path="/courses" component={Courses} />
+		</div>
+	);
 }
 
 export default App;
